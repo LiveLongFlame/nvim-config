0d41cb75c60772e80db6416396c8a2f0e48103b0
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-06-21 00:11:23 +1000

    added opening terminal to working file tree

--- lua/valen_nvim/remap.lua
+++ lua/valen_nvim/remap.lua
@@ -1,5 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ot", ":vsp<CR>:wincmd l<CR>:terminal<CR>", { noremap = true })
-
+vim.keymap.set("n", "<leader>ot", function()
+  local dir = vim.fn.expand('%:p:h')
+  vim.cmd("vsp")
+  vim.cmd("wincmd l")
+  vim.cmd("lcd " .. dir)       -- change local dir for the terminal window
+  vim.cmd("terminal")
+end, { noremap = true, silent = true })
 
