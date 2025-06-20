cda1a3d5c2d6d7d8b346064f2541eb191d3702dc
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-06-21 00:47:00 +1000

    added new commands for terminal

--- lua/valen_nvim/remap.lua
+++ lua/valen_nvim/remap.lua
@@ -1,13 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ht", function()
+vim.keymap.set("n", "<leader>jt", function()
   local dir = vim.fn.expand('%:p:h')
   vim.cmd("sp")
   vim.cmd("wincmd j")
   vim.cmd("lcd " .. dir)       -- change local dir for the terminal window
   vim.cmd("terminal")
 end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>vt", function()
+vim.keymap.set("n", "<leader>lt", function()
   local dir = vim.fn.expand('%:p:h')
   vim.cmd("vsp")
   vim.cmd("wincmd l")
