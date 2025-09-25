df573a352cc92290393ab4e39467167c87a9d389
Valentino Osorio Schwarz <valentinoosorioschwarz@gmail.com>
2025-09-26 02:13:01 +1000

    templates for cpp

---
+++ templates/cpp_template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define forn(i,n) for(int i =0; i < n;i++)
+#define fora(i,a) for(int i =0; i < a.size();i++)
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+
+	return 0;
+}
+
